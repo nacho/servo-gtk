f0e0fdc9e45ead67ec8f0d604e7e859ee255908f
Ignacio Casal Quinteiro <qignacio@amazon.com>
2025-09-29 12:13:07 +0200

    webview: resize servo to the size of the widget
    For some reason it is still not the right size but
    getting closer to the right one

--- src/webview.rs
+++ src/webview.rs
@@ -217,12 +217,12 @@ mod imp {
             });
 
             let obj_weak = self.obj().downgrade();
-            gl_area.connect_resize(move |_, width, height| {
+            gl_area.connect_resize(move |area, width, height| {
                 if let Some(obj) = obj_weak.upgrade() {
                     let imp = obj.imp();
 
                     if let Some(servo) = imp.servo_runner.borrow().as_ref() {
-                        servo.resize(width as u32, height as u32);
+                        servo.resize(area.width() as u32, area.height() as u32);
                     }
                 }
             });
