8c234944a8360ac8a40b72a4dcf43724d62389d5
Ignacio Casal Quinteiro <qignacio@amazon.com>
2025-10-06 17:21:45 +0200

    Convert WebView from GLArea to widget with snapshot override
    - Replace GLArea with normal widget that overrides snapshot function

--- servo_gtk/web_view.rs
+++ servo_gtk/web_view.rs
@@ -4,8 +4,8 @@
 
 use crate::proto_ipc::{ServoEvent, servo_event};
 use crate::servo_runner::{LogLevel, ServoRunner};
+use glib::info;
 use glib::translate::*;
-use glib::{info, warn};
 use gtk::gdk;
 use gtk::prelude::*;
 use gtk::{glib, subclass::prelude::*};
@@ -19,12 +19,8 @@ mod imp {
 
     #[derive(Default)]
     pub struct WebView {
-        pub gl_area: RefCell<Option<gtk::GLArea>>,
         pub servo_runner: RefCell<Option<ServoRunner>>,
-        pub last_image: RefCell<Option<RgbaImage>>,
-        pub shader_program: RefCell<u32>,
-        pub vao: RefCell<u32>,
-        pub texture: RefCell<u32>,
+        pub memory_texture: RefCell<Option<gdk::MemoryTexture>>,
     }
 
     #[glib::object_subclass]
@@ -32,238 +28,29 @@ mod imp {
         const NAME: &'static str = "WebView";
         type Type = super::WebView;
         type ParentType = gtk::Widget;
-
-        fn class_init(klass: &mut Self::Class) {
-            klass.set_layout_manager_type::<gtk::BinLayout>();
-        }
     }
 
     impl ObjectImpl for WebView {
         fn constructed(&self) {
             self.parent_constructed();
 
-            let gl_area = gtk::GLArea::new();
-            gl_area.set_focusable(true);
+            let servo_runner = ServoRunner::new();
+            let event_receiver = servo_runner.event_receiver();
+
+            self.servo_runner.replace(Some(servo_runner));
 
             let obj_weak = self.obj().downgrade();
-            gl_area.connect_realize(move |area| {
-                area.make_current();
-                gl::load_with(|name| epoxy::get_proc_addr(name) as *const _);
-
-                if area.uses_es() {
-                    info!("Using OpenGL ES");
-                }
-
-                if let Some(obj) = obj_weak.upgrade() {
-                    let imp = obj.imp();
-                    unsafe {
-                        // Create shader program
-                        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
-                        let vertex_source = if area.uses_es() {
-                            cr"#version 320 es
-                                precision highp float;
-                                layout (location = 0) in vec2 aPos;
-                                layout (location = 1) in vec2 aTexCoord;
-                                out vec2 TexCoord;
-                                void main() {
-                                    gl_Position = vec4(aPos, 0.0, 1.0);
-                                    TexCoord = aTexCoord;
-                                }"
-                        } else {
-                            cr"#version 330 core
-                                layout (location = 0) in vec2 aPos;
-                                layout (location = 1) in vec2 aTexCoord;
-                                out vec2 TexCoord;
-                                void main() {
-                                    gl_Position = vec4(aPos, 0.0, 1.0);
-                                    TexCoord = aTexCoord;
-                                }"
-                        };
-
-                        gl::ShaderSource(
-                            vertex_shader,
-                            1,
-                            &vertex_source.as_ptr(),
-                            std::ptr::null(),
-                        );
-                        gl::CompileShader(vertex_shader);
-                        let mut status: i32 = 0;
-                        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut status);
-                        if status == 0 {
-                            let mut buf: [u8; 1024] = [0; 1024];
-                            gl::GetShaderInfoLog(
-                                vertex_shader,
-                                1024,
-                                std::ptr::null_mut(),
-                                buf.as_mut_ptr().cast(),
-                            );
-                            warn!("INFO LOG {:?}", std::str::from_utf8(&buf[..]).unwrap());
-                        }
-
-                        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
-                        let fragment_source = if area.uses_es() {
-                            cr"#version 320 es
-                                precision highp float;
-                                out vec4 FragColor;
-                                in vec2 TexCoord;
-                                uniform sampler2D ourTexture;
-                                void main() {
-                                    FragColor = texture(ourTexture, TexCoord);
-                                }"
-                        } else {
-                            cr"#version 330 core
-                                out vec4 FragColor;
-                                in vec2 TexCoord;
-                                uniform sampler2D ourTexture;
-                                void main() {
-                                    FragColor = texture(ourTexture, TexCoord);
-                                }"
-                        };
-
-                        gl::ShaderSource(
-                            fragment_shader,
-                            1,
-                            &fragment_source.as_ptr(),
-                            std::ptr::null(),
-                        );
-                        gl::CompileShader(fragment_shader);
-                        let mut status: i32 = 0;
-                        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut status);
-                        if status == 0 {
-                            let mut buf: [u8; 1024] = [0; 1024];
-                            gl::GetShaderInfoLog(
-                                fragment_shader,
-                                1024,
-                                std::ptr::null_mut(),
-                                buf.as_mut_ptr().cast(),
-                            );
-                            warn!("INFO LOG {:?}", std::str::from_utf8(&buf[..]).unwrap());
-                        }
-
-                        let program = gl::CreateProgram();
-                        gl::AttachShader(program, vertex_shader);
-                        gl::AttachShader(program, fragment_shader);
-                        gl::LinkProgram(program);
-                        gl::DeleteShader(vertex_shader);
-                        gl::DeleteShader(fragment_shader);
-
-                        imp.shader_program.replace(program);
-
-                        // Create VAO and VBO
-                        let vertices: [f32; 16] = [
-                            -1.0, -1.0, 0.0, 1.0, // bottom left
-                            1.0, -1.0, 1.0, 1.0, // bottom right
-                            1.0, 1.0, 1.0, 0.0, // top right
-                            -1.0, 1.0, 0.0, 0.0, // top left
-                        ];
-                        let indices: [u32; 6] = [0, 1, 2, 2, 3, 0];
-
-                        let mut vao = 0;
-                        let mut vbo = 0;
-                        let mut ebo = 0;
-                        gl::GenVertexArrays(1, &mut vao);
-                        gl::GenBuffers(1, &mut vbo);
-                        gl::GenBuffers(1, &mut ebo);
-
-                        gl::BindVertexArray(vao);
-                        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-                        gl::BufferData(
-                            gl::ARRAY_BUFFER,
-                            (vertices.len() * std::mem::size_of::<f32>()) as isize,
-                            vertices.as_ptr() as *const _,
-                            gl::STATIC_DRAW,
-                        );
-
-                        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-                        gl::BufferData(
-                            gl::ELEMENT_ARRAY_BUFFER,
-                            (indices.len() * std::mem::size_of::<u32>()) as isize,
-                            indices.as_ptr() as *const _,
-                            gl::STATIC_DRAW,
-                        );
-
-                        gl::VertexAttribPointer(
-                            0,
-                            2,
-                            gl::FLOAT,
-                            gl::FALSE,
-                            4 * std::mem::size_of::<f32>() as i32,
-                            std::ptr::null(),
-                        );
-                        gl::EnableVertexAttribArray(0);
-                        gl::VertexAttribPointer(
-                            1,
-                            2,
-                            gl::FLOAT,
-                            gl::FALSE,
-                            4 * std::mem::size_of::<f32>() as i32,
-                            (2 * std::mem::size_of::<f32>()) as *const _,
-                        );
-                        gl::EnableVertexAttribArray(1);
-
-                        imp.vao.replace(vao);
-
-                        // Create texture
-                        let mut texture = 0;
-                        gl::GenTextures(1, &mut texture);
-                        imp.texture.replace(texture);
+            glib::spawn_future_local(async move {
+                while let Ok(event) = event_receiver.recv().await {
+                    if let Some(obj) = obj_weak.upgrade() {
+                        obj.process_servo_event(event);
+                    } else {
+                        break;
                     }
                 }
             });
 
-            let obj_weak = self.obj().downgrade();
-            gl_area.connect_render(move |_, _| {
-                if let Some(obj) = obj_weak.upgrade() {
-                    let imp = obj.imp();
-                    if let Some(rgba_image) = imp.last_image.borrow().as_ref() {
-                        unsafe {
-                            gl::Clear(gl::COLOR_BUFFER_BIT);
-
-                            // Update texture
-                            gl::BindTexture(gl::TEXTURE_2D, *imp.texture.borrow());
-                            gl::TexImage2D(
-                                gl::TEXTURE_2D,
-                                0,
-                                gl::RGBA as i32,
-                                rgba_image.width() as i32,
-                                rgba_image.height() as i32,
-                                0,
-                                gl::RGBA,
-                                gl::UNSIGNED_BYTE,
-                                rgba_image.as_raw().as_ptr() as *const _,
-                            );
-                            gl::TexParameteri(
-                                gl::TEXTURE_2D,
-                                gl::TEXTURE_MIN_FILTER,
-                                gl::LINEAR as i32,
-                            );
-                            gl::TexParameteri(
-                                gl::TEXTURE_2D,
-                                gl::TEXTURE_MAG_FILTER,
-                                gl::LINEAR as i32,
-                            );
-
-                            // Render
-                            gl::UseProgram(*imp.shader_program.borrow());
-                            gl::BindVertexArray(*imp.vao.borrow());
-                            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
-                        }
-                    }
-                }
-                glib::Propagation::Stop
-            });
-
-            let obj_weak = self.obj().downgrade();
-            gl_area.connect_resize(move |area, _width, _height| {
-                if let Some(obj) = obj_weak.upgrade() {
-                    let imp = obj.imp();
-
-                    if let Some(servo) = imp.servo_runner.borrow().as_ref() {
-                        servo.resize(area.width() as u32, area.height() as u32);
-                    }
-                }
-            });
-
+            // Event controllers
             let motion_controller = gtk::EventControllerMotion::new();
             let obj_weak = self.obj().downgrade();
             motion_controller.connect_motion(move |_, x, y| {
@@ -274,7 +61,7 @@ mod imp {
                     }
                 }
             });
-            gl_area.add_controller(motion_controller);
+            self.obj().add_controller(motion_controller);
 
             let legacy_controller = gtk::EventControllerLegacy::new();
             let obj_weak = self.obj().downgrade();
@@ -317,7 +104,7 @@ mod imp {
                 }
                 glib::Propagation::Proceed
             });
-            gl_area.add_controller(legacy_controller);
+            self.obj().add_controller(legacy_controller);
 
             let key_controller = gtk::EventControllerKey::new();
             let obj_weak = self.obj().downgrade();
@@ -344,12 +131,8 @@ mod imp {
                     }
                 }
             });
-            gl_area.add_controller(key_controller);
+            self.obj().add_controller(key_controller);
 
-            // FIXME: ideally we would do some proper size measuring so
-            // we can embed the webview in a scrolled window. Checking the api
-            // we do not seem to get any notification of the page size so
-            // I don't think this can be done for now
             let scroll_controller =
                 gtk::EventControllerScroll::new(gtk::EventControllerScrollFlags::BOTH_AXES);
             let obj_weak = self.obj().downgrade();
@@ -362,44 +145,9 @@ mod imp {
                 }
                 glib::Propagation::Stop
             });
-            gl_area.add_controller(scroll_controller);
-
-            gl_area.set_parent(&*self.obj());
-            self.gl_area.replace(Some(gl_area));
-
-            let servo_runner = ServoRunner::new();
-            let event_receiver = servo_runner.event_receiver();
-
-            self.servo_runner.replace(Some(servo_runner));
-
-            let obj_weak = self.obj().downgrade();
-            glib::spawn_future_local(async move {
-                while let Ok(event) = event_receiver.recv().await {
-                    if let Some(obj) = obj_weak.upgrade() {
-                        obj.process_servo_event(event);
-                    } else {
-                        break;
-                    }
-                }
-            });
-
-            // Note the focus controller is added on the webview since
-            // we want to proxy the focus the gl_area whenever something
-            // tries to grab the focus in the webview
-            let focus_controller = gtk::EventControllerFocus::new();
-            let obj_weak = self.obj().downgrade();
-            focus_controller.connect_enter(move |_| {
-                if let Some(obj) = obj_weak.upgrade() {
-                    let imp = obj.imp();
-                    if let Some(gl_area) = imp.gl_area.borrow().as_ref() {
-                        gl_area.grab_focus();
-                    }
-                }
-            });
-            self.obj().add_controller(focus_controller);
+            self.obj().add_controller(scroll_controller);
 
             self.obj().set_focusable(true);
-
             info!("Webview constructed");
         }
 
@@ -410,7 +158,25 @@ mod imp {
         }
     }
 
-    impl WidgetImpl for WebView {}
+    impl WidgetImpl for WebView {
+        fn snapshot(&self, snapshot: &gtk::Snapshot) {
+            if let Some(texture) = self.memory_texture.borrow().as_ref() {
+                let bounds = gtk::graphene::Rect::new(
+                    0.0,
+                    0.0,
+                    self.obj().width() as f32,
+                    self.obj().height() as f32,
+                );
+                snapshot.append_texture(texture, &bounds);
+            }
+        }
+
+        fn size_allocate(&self, width: i32, height: i32, _baseline: i32) {
+            if let Some(servo) = self.servo_runner.borrow().as_ref() {
+                servo.resize(width as u32, height as u32);
+            }
+        }
+    }
 }
 
 glib::wrapper! {
@@ -462,11 +228,8 @@ impl WebView {
         let event_x = event_x - nx;
         let event_y = event_y - ny;
 
-        let gl_area = self.imp().gl_area.borrow();
-        let gl_area = gl_area.as_ref()?;
-
         let point = gtk::graphene::Point::new(event_x as f32, event_y as f32);
-        let translated = root.compute_point(gl_area, &point)?;
+        let translated = root.compute_point(self, &point)?;
 
         Some((translated.x() as f64, translated.y() as f64))
     }
@@ -477,9 +240,6 @@ impl WebView {
         };
 
         match event_type {
-            // FIXME: this is just a hack to get me going. Ideally we would
-            // use a DMA-Buf so we avoid movign the pixels from the GPU to
-            // system memory and back to the GPU
             servo_event::Event::FrameReady(frame_ready) => {
                 let rgba_image = RgbaImage::from_raw(
                     frame_ready.width,
@@ -487,30 +247,35 @@ impl WebView {
                     frame_ready.rgba_data,
                 )
                 .unwrap();
+
                 let imp = self.imp();
 
-                imp.last_image.replace(Some(rgba_image));
+                let bytes = glib::Bytes::from(&rgba_image.as_raw()[..]);
+                let texture = gdk::MemoryTexture::new(
+                    rgba_image.width() as i32,
+                    rgba_image.height() as i32,
+                    gdk::MemoryFormat::R8g8b8a8,
+                    &bytes,
+                    (rgba_image.width() * 4) as usize,
+                );
 
-                if let Some(gl_area) = imp.gl_area.borrow().as_ref() {
-                    gl_area.queue_draw();
-                }
+                imp.memory_texture.replace(Some(texture));
+                self.queue_draw();
             }
             servo_event::Event::CursorChanged(cursor_changed) => {
                 let gdk_cursor = gdk::Cursor::from_name(&cursor_changed.cursor, None);
-
                 if let Some(cursor) = gdk_cursor {
                     self.set_cursor(Some(&cursor));
                 }
             }
             servo_event::Event::LogMessage(log_msg) => {
-                // Process log message and forward to servo_runner
                 if let Some(servo_runner) = self.imp().servo_runner.borrow().as_ref() {
                     servo_runner
                         .handle_log_message(LogLevel::from(log_msg.level), &log_msg.message);
                 }
             }
             _ => {
-                // Handle other event types as needed
+                info!("Unhandled event type: {:?}", event_type);
             }
         }
     }
